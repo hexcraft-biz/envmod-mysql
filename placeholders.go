7ab90b07fe636345bd12aef568d739895ef8797e
Boss <cure1218@gmail.com>
2023-11-12 15:20:40 +0000

    boss

--- placeholders.go
+++ placeholders.go
@@ -1,6 +1,6 @@
 package mysql
 
-func PositionalPlaceholdersSlice(num int) []string {
+func PositionalPlaceholders(num int) []string {
 	phs := make([]string, num)
 	for i := range phs {
 		phs[i] = "?"
